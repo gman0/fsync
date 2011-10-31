419fe5f3ac1e9b018b60d6e202be982680f8466a
gman <gman@gmans-magicbox>
2011-10-31 11:09:45 +0100

    all #define will be stored in defs.h

---
+++ src/defs.h
@@ -0,0 +1,31 @@
+/*
+	Copyright (C) 2011 Róbert Vašek <gman@codefreax.org>
+
+    This file is part of fsync.
+
+    fsync is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    fsync is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fsync.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DEFS_H
+#define DEFS_H
+
+#define HASH_LENGTH 16
+#define CHUNK_SIZE 2048
+
+#define BLOCK_SIZE 32448
+#define HEADER_BUFF_SIZE 512
+
+#define PATH_LENGTH 340
+
+#endif // DEFS_H
