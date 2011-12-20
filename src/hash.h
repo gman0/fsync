d797b2b2ca971f46a8050dfdf58c24c0affddf0c
gman <gman@codefreax.org>
2011-12-20 12:51:37 +0100

    changed hash algorithm

--- src/hash.h
+++ src/hash.h
@@ -1,39 +1,42 @@
-/*
- * downloaded from <http://www.azillionmonkeys.com/qed/hash.html>
- * 
- * Copyright 2004-2008 by Paul Hsieh
- * 
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation, either version 2.1 of the License, or
- *  (at your option) any later version.
- * 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+//-----------------------------------------------------------------------------
+// MurmurHash3 was written by Austin Appleby, and is placed in the public
+// domain. The author hereby disclaims copyright to this source code.
 
-#ifndef HASH_H
-#define HASH_H
+#ifndef _MURMURHASH3_H_
+#define _MURMURHASH3_H_
 
-#include <cstdlib>
+//-----------------------------------------------------------------------------
+// Platform-specific functions and macros
+
+// Microsoft Visual Studio
+
+#if defined(_MSC_VER)
+
+typedef unsigned char uint8_t;
+typedef unsigned long uint32_t;
+typedef unsigned __int64 uint64_t;
+
+// Other compilers
+
+#else	// defined(_MSC_VER)
+
 #include <stdint.h>
 
-#undef get16bits
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-#define get16bits(d) (*((const uint16_t *) (d)))
-#endif
+#endif // !defined(_MSC_VER)
 
-#if !defined (get16bits)
-#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
-                       +(uint32_t)(((const uint8_t *)(d))[0]) )
-#endif
+//-----------------------------------------------------------------------------
 
-uint32_t calculateHash(const unsigned char * data, size_t len);
+void MurmurHash3_x86_32  ( const void * key, int len, uint32_t seed, void * out );
 
-#endif // HASH_H
+void MurmurHash3_x86_128 ( const void * key, int len, uint32_t seed, void * out );
+
+void MurmurHash3_x64_128 ( const void * key, int len, uint32_t seed, void * out );
+
+//-----------------------------------------------------------------------------
+
+
+uint32_t calculateHash(const unsigned char * data, int len);
+
+
+#endif // _MURMURHASH3_H_
+
