9eda16b003e0eaf9303566f82e595fc6a8a85572
gman <gman@codefreax.org>
2012-01-08 13:53:10 +0100

    Server's main is now using class Server for the application part

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -18,15 +18,8 @@
 */
 
 #include <iostream>
-#include <fstream>
-#include "Config.h"
 #include "FSException.h"
-#include "LogManager.h"
-#include "NetworkManager.h"
-#include "Packet.h"
-#include "PacketContainer.h"
-
-#include "FileGatherer.h"
+#include "Server.h"
 
 using namespace std;
 
@@ -34,59 +27,7 @@ int main(int argc, char * argv[])
 {
 	try
 	{
-		/*======== TESTING =========*/
-/*
-		const char * fileName = "test.rar";
-
-		ifstream file(fileName, ifstream::in);
-
-		PacketHeader_File ph_file;
-		memcpy(ph_file.m_path, fileName, strlen(fileName));
-
-		PacketContainer pc(1, PCKT_FILE, sizeof(ph_file), (unsigned char*)&ph_file, file);
-
-*/
-		/*======== TESTING =========*/
-
-		LogManager::instance("fsync_server.log");
-
-		Config config(argc, (const char**)argv, "server.conf");
-		//unsigned int port = config.getPort();
-		ID_Path_pairList pathList = config.getPathList();
-
-		for (ID_Path_pairList::const_iterator i = pathList.begin(); i != pathList.end(); i++)
-		{
-			cout << i->first << ": " << i->second << endl;
-		}
-
-		cout << "===\n\n";
-
-		//FileGatherer fg(&config);
-/*
-		NetworkManager networkManager((port) ? port : 2000); // FIXME: see Valgrind output
-		networkManager.openSocket();
-
-		while (1)
-		{
-			break;
-			if (networkManager.acceptConnection())
-				cout << "client connected\n";
-			else
-			{
-				SDL_Delay(100);
-				continue;
-			}
-
-			IPaddress * ip = networkManager.getClientAddress();
-
-			if (ip)
-				cout << "Host connected: " << SDLNet_Read32(&ip->host) << " " << SDLNet_Read16(&ip->port) << endl;
-
-			networkManager.closeConnection();
-		}
-*/
-
-		LogManager::getInstancePtr()->destroy();
+		Server app(argc, argv);
 	}
 	catch (FSException & e)
 	{
