4af59f608c6ea5a7493a672535bd95e511179f8b
gman <gman@codefreax.org>
2012-01-08 13:06:37 +0100

    Client's main is now using class Client for the application part

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,45 +1,14 @@
 #include <iostream>
-#include <cstring>
-#include <fstream>
-#include "Config.h"
+#include "Client.h"
 #include "FSException.h"
-#include "LogManager.h"
-#include "NetworkManager.h"
 
 using namespace std;
 
 int main(int argc, char * argv[])
 {
-	int len;
-	ifstream file;
-	file.open("test.zip", ifstream::binary);
-
-	file.seekg(0, ifstream::end);
-	len = file.tellg();
-	file.seekg(0, ifstream::beg);
-
-	char buffer[len];
-	file.read(buffer, len);
-
-	Packet p;
-	p.m_type = PCKT_FILE_CHUNK;
-	memcpy(p.m_buffer, buffer, len);
-	p.len = len;
-
-	file.close();
-
 	try
 	{
-		LogManager::instance("fsync_client.log");
-
-		NetworkManager networkManager("localhost", 2000);
-		networkManager.openSocket();
-
-		networkManager.send(p);
-
-		cin.get();
-
-		networkManager.closeConnection();
+		Client app(argc, argv);
 	}
 	catch (FSException & e)
 	{
