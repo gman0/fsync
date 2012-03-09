fbb598c192503f7382f57991e90c7136269a2c20
gman <gman@codefreax.org>
2012-03-09 23:52:09 +0100

    Added Exit exception class

---
+++ src/Exit.h
@@ -0,0 +1,32 @@
+/*
+	Copyright (C) 2011 Róbert Vašek <gman@codefreax.org>
+
+    This file is part of fsync.
+
+    fsync is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    fsync is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fsync.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EXIT_H
+#define EXIT_H
+
+#include <exception>
+
+class Exit : public std::exception
+{
+	public:
+		const char * what() const throw() { return 0; }
+		~Exit() throw() {}
+};
+
+#endif // EXIT_H
